4e69de8c296ea22f47296c99aa1c1f7d7c0e17b9
darknesside <lou-theo.laurent@laposte.net>
2016-05-05 13:32:45 +0200

    Added files via upload

---
+++ alpha 0.0/root.py
@@ -0,0 +1,27 @@
+# Dans tout le programme le numéro des faces et des cases restent identique : c'est un repère en coordonnées absolue
+
+from tkinter import *
+from random import *
+from modules.affichage import *
+from modules.mouvements import *
+
+
+
+# initialisation de la liste, les faces vont de 1 à 5 (selon leur indice) et chaque case d'une face est numéroté de 0 à 8
+cube=[["green"]*9,["red"]*9,["blue"]*9,["orange"]*9,["white"]*9,["yellow"]*9]
+
+main=Tk()
+
+can=Canvas(main,bg="white",height=450,width=600)
+can.pack()
+
+actualise(cube,can)
+bou1=Button(main, text="mélanger", command=lambda:melangeur(cube,can,5), fg='blue')
+bou1.pack()
+
+# pour que tu puisses tester tes trucs. Pour utiliser mes fonctions ensuite il faudra que tu prennes "cube" et "can" en argument
+bou2=Button(main, text="gauche face centre", command=lambda:rotation(cube,can,1,"droite"), fg='blue')
+bou2.pack()
+
+main.mainloop()
+main.destroy()
